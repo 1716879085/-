9a2d60398cd586bfa0149d9fb8a537ceb9154dfd
hql <1716879085@qq.com>
2022-06-07 17:59:07 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+def lossfunciton(b, w, points):
+    cnt = 0
+    for i in range(0, len(points)):
+        x = points[i, 0]
+        y = points[i, 1]
+        cnt += (y - (w * x + b)) ** 2
+    return cnt / float(len(points))
+
+def gradient(b,w,points,learningRate):
+    b = 0
+    
